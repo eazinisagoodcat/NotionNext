9e8a08040506046707e738bbaa5ae58e1422bb13
eazinisagoodcat <hionelittlestar@proton.me>
2025-04-03 21:37:02 +0800

    Update style.js

--- themes/fukasawa/style.js
+++ themes/fukasawa/style.js
@@ -29,7 +29,10 @@ const Style = () => {
 
     #starry-sky-vixcity {
     position: fixed !important; /* 让它固定但覆盖整个页面 */
+    height: 100vh; /* 确保覆盖整个视口 */
     z-index: 9999 !important;
+    pointer-events: none; /* 避免影响鼠标交互 */
+
 }
     
 
